2b79497511cab5af3f11cf4366ec60eceb471f72
x0Z3ro0x <x0Z3ro0x@users.noreply.github.com>
2020-08-27 11:29:48 -0700

    Update online.js
    We caught NuggetBot offline again, now the picture and command is complete.

--- Commands/online.js
+++ Commands/online.js
@@ -6,7 +6,7 @@ module.exports = {
     name: "online",
     description: "A joke command to check if the bot is online",
     execute(msg, args) {
-        msg.channel.send("Did you really just ask me that <@" + msg.author.id + ">? Yes I am online and working as intended, who do you think I am, Nugget Bot? :rolling_eyes:", { files: ["https://i.ibb.co/1Xsmg85/Capture.png"] });
+        msg.channel.send("Did you really just ask me that <@" + msg.author.id + ">? Yes I am online and working as intended, who do you think I am, Nugget Bot? :rolling_eyes:", { files: ["https://i.ibb.co/xFVbqQv/Capture3.png"] });
         msg.client.channels.cache.get("615509476317069315").send(msg.author.username + " just tried using command - ```" + cmdPrefix + "online```");
     }
 };
