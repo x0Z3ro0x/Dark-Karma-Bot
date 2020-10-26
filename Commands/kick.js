47c0cb95966e3f7b3d6117207ce5a6042f21e8cd
x0Z3ro0x <x0Z3ro0x@users.noreply.github.com>
2020-10-26 06:56:45 -0700

    Update kick.js
    Enhanced the kick command giving it more functionality. I also removed the kickable roles and replaced them with tiers.
    
    If the person kicking, has a rank lower than the person being kicked, it will reject the command. This is more efficient than listing which roles not to kick.

--- Commands/kick.js
+++ Commands/kick.js
@@ -10,30 +10,35 @@ module.exports = {
         
         if (member.roles.cache.has("615380530480939027") || member.roles.cache.has("615507860389888020") || member.roles.cache.has("615507508668137493") || member.roles.cache.has("615508127948996608") || member.roles.cache.has("615508552165097524") || member.roles.cache.has("664208918133735450")) {
             var mentionMember = msg.mentions.members.first();
+            var fighter1 = msg.member.roles.highest;
 
             if (!mentionMember) {
                 msg.delete();
                 msg.channel.send("You need to give me a user to kick!").then(msg => {
                     msg.delete({ timeout:12000 })});
+                return;
+            }
+
+            var userID = mentionMember.user.id;
+            var userName = mentionMember.user.username;
+            var fighter2 = mentionMember.roles.highest;
+
+            if (fighter2.rawPosition >= fighter1.rawPosition) {
+                msg.delete();
+                msg.channel.send(userName + " is above you. You may only kick users beneath your user group.").then(msg => {
+                    msg.delete({ timeout:12000 })});
             }
             else if (!mentionMember.kickable) {
                 msg.delete();
-                msg.channel.send("I cannot kick that user...").then(msg => {
-                    msg.delete({ timeout:12000 })});
-            }
-            else if (member.roles.cache.has("615380530480939027") || member.roles.cache.has("615507860389888020") || member.roles.cache.has("615507508668137493") || member.roles.cache.has("615508127948996608") || member.roles.cache.has("615508552165097524") || member.roles.cache.has("664208918133735450")) {
-                msg.delete();
-                msg.channel.send("Did you really think I was dumb enough to let you kick out staff? Remove their tag first if this is a legit kick.").then(msg => {
+                msg.channel.send("Woah! That user is far to strong for me to kick!").then(msg => {
                     msg.delete({ timeout:12000 })});
             }
             else {
-                var userID = mentionMember.user.id;
                 msg.delete();
                 msg.channel.send("<@" + userID + "> has been steam rolled out of the Discord server. Bye Felicia.").then(msg => {
                     msg.delete({ timeout:12000 })});
                 mentionMember.kick();
             }
-            //msg.channel.send("Test bot is online. Are you ready to do some work sir?");
         }
         else {
             msg.delete();
