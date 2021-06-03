c8006740c031a4fa2a8fe076c0b2dc3c1c433a92
x0Z3ro0x <x0Z3ro0x@users.noreply.github.com>
2021-06-03 11:43:33 -0700

    Update
    Refined who can use admin commands. Don't want the wrong people using admin commands now that the bot is gaining power to harm.

--- Commands/admindetailedhelp.js
+++ Commands/admindetailedhelp.js
@@ -6,7 +6,16 @@ module.exports = {
     name: "detailedadminhelp",
     description: "A advanced help command that breaks down what each admin command does",
     execute(msg, args) {
-        if (member.roles.cache.has("615380530480939027") || member.roles.cache.has("615507860389888020") || member.roles.cache.has("615507508668137493") || member.roles.cache.has("615508127948996608") || member.roles.cache.has("615508552165097524") || member.roles.cache.has("664208918133735450")) {
+        // 615380530480939027 = OG
+        // 615508552165097524 = Staff
+        // 615507860389888020 = Design Developer
+        // 615507508668137493 = Code Developer
+        // 761721046172041216 = Manager
+        // 664208918133735450 = Support Guru
+
+        const member = msg.member;
+
+        if (member.roles.cache.has("615380530480939027") || member.roles.cache.has("615508552165097524") || member.roles.cache.has("615507860389888020") || member.roles.cache.has("615507508668137493") || member.roles.cache.has("761721046172041216") || member.roles.cache.has("664208918133735450")) {
             msg.delete();
             msg.channel.send("Here is a detailed list of admin commands -\n\n```*adminhelp``` - Shows you all possible admin commands\n\n```*detailedadminhelp``` - Shows advanced info on admin commands" +
                 "\n\n```*kick``` - Kicks a mentioned user\n\n```*ban``` - Bans a mentioned user\n\n```*purge #``` - Purges a specified number of messages\n\n```*new_gameassignment``` - Posts a new " +
--- Commands/adminhelp.js
+++ Commands/adminhelp.js
@@ -6,9 +6,16 @@ module.exports = {
     name: "adminhelp",
     description: "A simple help command for admins.",
     execute(msg, args) {
+        // 615380530480939027 = OG
+        // 615508552165097524 = Staff
+        // 615507860389888020 = Design Developer
+        // 615507508668137493 = Code Developer
+        // 761721046172041216 = Manager
+        // 664208918133735450 = Support Guru
+
         const member = msg.member;
 
-        if (member.roles.cache.has("615380530480939027") || member.roles.cache.has("615507860389888020") || member.roles.cache.has("615507508668137493") || member.roles.cache.has("615508127948996608") || member.roles.cache.has("615508552165097524") || member.roles.cache.has("664208918133735450")) {
+        if (member.roles.cache.has("615380530480939027") || member.roles.cache.has("615508552165097524") || member.roles.cache.has("615507860389888020") || member.roles.cache.has("615507508668137493") || member.roles.cache.has("761721046172041216") || member.roles.cache.has("664208918133735450")) {
             msg.delete();
             msg.channel.send("Currently, there are 9 possible admin commands - \n```\n*adminhelp\n*detailedadminhelp\n*kick\n*ban\n*purge\n*new_gameassignment\n*new_helpme\n*new_veil\n*new_welcome```").then(msg => {
                 msg.delete({ timeout: 21000 })
